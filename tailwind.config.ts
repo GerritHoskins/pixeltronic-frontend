bc518bb328f365167ca9d5fa64ea116fed3ba740
gerrithoskins <gerrit.hoskins@gmail.com>
2023-10-27 19:20:46 +0200

    fix tailwind config and other stuff

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,70 +1,24 @@
-import type { Config } from 'tailwindcss';
-import { fontFamily } from 'tailwindcss/defaultTheme';
+import { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
-import plugin from '@tailwindcss/forms';
 
-const config: Config = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+import { fontFamily } from 'tailwindcss/defaultTheme';
+import defineTailwindConfig from './src/config';
+
+const config: Config = defineTailwindConfig({
+  content: ['./src/**/*.{js,vue,ts}', './index.html'],
   darkMode: 'class',
   theme: {
     extend: {
-      lineHeight: {
-        11: '2.75rem',
-        12: '3rem',
-        13: '3.25rem',
-        14: '3.5rem',
-      },
       fontFamily: {
-        //sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
-        sans: ['"Sora-Light"', ...fontFamily.sans],
+        sans: ['Sora', ...fontFamily.sans],
       },
       colors: {
         primary: colors.pink,
         gray: colors.gray,
       },
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.600')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2': {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-            },
-            h3: {
-              fontWeight: '600',
-            },
-            code: {
-              color: theme('colors.indigo.500'),
-            },
-          },
-        },
-        invert: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.400')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2,h3,h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
-          },
-        },
-      }),
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [plugin],
-};
+  plugins: [],
+});
 
 export default config;
