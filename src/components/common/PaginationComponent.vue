760832ce13342c37a894d3b8fbfd9f1526c65633
gerrithoskins <gerrit.hoskins@gmail.com>
2023-10-29 19:25:12 +0100

    merge apis, added loading spinner, reworked some components

---
+++ src/components/common/PaginationComponent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="space-y-2 pb-8 pt-6 md:space-y-5">
+    <nav class="flex justify-between">
+      <button class="cursor-auto disabled:opacity-50">Previous</button><span>1 of 2</span
+      ><a rel="next" href="/blog/page/2">Next</a>
+    </nav>
+  </div>
+</template>
